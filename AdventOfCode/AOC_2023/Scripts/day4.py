4c92223cb7b4fb6955f71a61cfe248602ce7f692
pepperbel <pepperbel@hotmail.com>
2023-12-04 22:36:07 +0100

    day 4 part 2

--- AdventOfCode/AOC_2023/Scripts/day4.py
+++ AdventOfCode/AOC_2023/Scripts/day4.py
@@ -16,28 +16,27 @@ TESTCASES:list[str] = [
 
 # endregion -------------------------
 
-class RowAttributes(object):
+class Scratchcard(object):
 
     def __init__(self, row:str) -> None:
-        self.game_id_str:str = self._get_card_id(row.split(":")[0])
-        self.game_id_index:int = int(self.game_id_str) - 1
-        self.lottery_numbers:list[int] = self._get_numbers_from_string(row.split(":")[1].split("|")[0])
+        self.game_id:str = self._get_card_id(row.split(":")[0])
+        self.win_key_numbers:list[int] = self._get_numbers_from_string(row.split(":")[1].split("|")[0])
         self.input_numbers:list[int] = self._get_numbers_from_string(row.split(":")[1].split("|")[1])
 
         self.winning_numbers:list[int] = self._get_winning_numbers()
         self.card_value:int = self._get_card_value()
 
-        self.possible_copies:list[int] = self._get_card_copy_ids()
+        self.copies_created:int = 1
 
     def _get_card_id(self, full_str:str) -> str:
         return full_str.split(" ")[1].strip()
 
     def _get_numbers_from_string(self, digits:str) -> list[int]:
         number_array = digits.split(" ")
-        return [int(item) for item in number_array if item != '']
+        return [int(number) for number in number_array if number != '']
     
     def _get_winning_numbers(self) -> list[int]:
-        set1 = set(self.lottery_numbers)
+        set1 = set(self.win_key_numbers)
         set2 = set(self.input_numbers)
         common_elements = list(set1.intersection(set2))
         return common_elements
@@ -51,57 +50,44 @@ class RowAttributes(object):
         else:
             return 0
 
-    def _get_card_copy_ids(self) -> list[int]:
-        # Returns an index for a list, in oprder to get string value for card, need to +1 to all
-        # Card 1 will have an index of 0
-        num_copies = len(self.winning_numbers)
-        id_list:list[int] = []
-        for i in range(0, num_copies):
-            id_list.append(self.game_id_index + i + 1)
-
-        return id_list
-
     def DEBUG_print_attributes(self):
         print("-------- IDs and Copies --------")
-        print("GAME ID STR: ", self.game_id_str)
-        print("GAME ID INDEX: ", self.game_id_index)
-        print("COPY INDEX IDs: ", self.possible_copies)
+        print("GAME ID STR: ", self.game_id)
         print("-------- Numbers on Card --------")
-        print("LOTTERY NUMS: ", self.lottery_numbers)
+        print("WIN KEY NUMS: ", self.win_key_numbers)
         print("INPUT NUMS: ", self.input_numbers)
         print("-------- Winning Part 1 --------")
         print("WINNING NUMS: ", self.winning_numbers)
         print("CARD VALUE: ", self.card_value)
 
-def get_row_attributes(id:int):
-    return RowAttributes(INPUT[id])
+# ----------------- MAIN --------------------- #
 
+def get_scratchcards(input:list[str]) -> list[Scratchcard]:
+    return [Scratchcard(row) for row in input]
 
-card_value_part_1 = 0
-list_of_copies = []
-scratchcard_count_part_2 = 0
-win = True
+def execute_day_4() -> tuple(int):
 
-for row in TESTCASES:
-    row_attr:RowAttributes = RowAttributes(row)
-    card_value_part_1 += row_attr.card_value
-    row_attr.DEBUG_print_attributes()
+    counter_part_1 = 0
+    counter_part_2 = 0
+    scratchcards:list[Scratchcard] = get_scratchcards(INPUT)
 
-    # PART 2
-    num_copies = len(row_attr.winning_numbers)
-    while win:
-        if num_copies > 0:
-            num_copies -= 1
+    for current_card in scratchcards:
 
-            list_of_copies += 
+        #PART 1
+        counter_part_1 += current_card.card_value
 
-        else:
-            win = False
+        # PART 2
+        for i in range(len(current_card.winning_numbers)):
+            index = scratchcards.index(current_card) + 1 + i
+            scratchcards[index].copies_created += current_card.copies_created
 
-print("DAY 4 Part 1: ", card_value_part_1) # ANSWER == 18619
-print("DAY 4 Part 2: ", card_value_part_1) # ANSWER == 
+    for current_card in scratchcards:
+        counter_part_2 += current_card.copies_created
+    
+    return counter_part_1, counter_part_2
 
-
-# 1. get a list of all possible copies and originals
+part_1, part_2 = execute_day_4()
+print("DAY 4 Part 1: ", part_1) # ANSWER == 18619
+print("DAY 4 Part 2: ", part_2) # ANSWER == 8063216
 
 
